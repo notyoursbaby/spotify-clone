00285a1736ea93639c01b86a3156cb2ee7aedf3e
notyoursbaby <167247131+notyoursbaby@users.noreply.github.com>
2025-06-23 18:50:42 +0530

    Create mockSongs.js

---
+++ src/data/mockSongs.js
@@ -0,0 +1,84 @@
+export const mockSongs = [
+  {
+    id: 1,
+    title: "Blinding Lights",
+    artist: "The Weeknd",
+    album: "After Hours",
+    duration: "3:20",
+    image: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=300&h=300&fit=crop",
+    genre: "Pop",
+    audioUrl: "https://www.soundjay.com/misc/sounds/bell-ringing-05.wav"
+  },
+  {
+    id: 2,
+    title: "Watermelon Sugar",
+    artist: "Harry Styles",
+    album: "Fine Line",
+    duration: "2:54",
+    image: "https://images.unsplash.com/photo-1471478331149-c72f17e33c73?w=300&h=300&fit=crop",
+    genre: "Pop",
+    audioUrl: "https://www.soundjay.com/misc/sounds/bell-ringing-05.wav"
+  },
+  {
+    id: 3,
+    title: "Good 4 U",
+    artist: "Olivia Rodrigo",
+    album: "SOUR",
+    duration: "2:58",
+    image: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=300&h=300&fit=crop",
+    genre: "Pop Rock",
+    audioUrl: "https://www.soundjay.com/misc/sounds/bell-ringing-05.wav"
+  },
+  {
+    id: 4,
+    title: "Levitating",
+    artist: "Dua Lipa",
+    album: "Future Nostalgia",
+    duration: "3:23",
+    image: "https://images.unsplash.com/photo-1471478331149-c72f17e33c73?w=300&h=300&fit=crop",
+    genre: "Dance Pop",
+    audioUrl: "https://www.soundjay.com/misc/sounds/bell-ringing-05.wav"
+  },
+  {
+    id: 5,
+    title: "Stay",
+    artist: "The Kid LAROI & Justin Bieber",
+    album: "F*CK LOVE 3",
+    duration: "2:21",
+    image: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=300&h=300&fit=crop",
+    genre: "Hip Hop",
+    audioUrl: "https://www.soundjay.com/misc/sounds/bell-ringing-05.wav"
+  },
+  {
+    id: 6,
+    title: "Peaches",
+    artist: "Justin Bieber ft. Daniel Caesar",
+    album: "Justice",
+    duration: "3:18",
+    image: "https://images.unsplash.com/photo-1471478331149-c72f17e33c73?w=300&h=300&fit=crop",
+    genre: "R&B",
+    audioUrl: "https://www.soundjay.com/misc/sounds/bell-ringing-05.wav"
+  },
+  {
+    id: 7,
+    title: "Heat Waves",
+    artist: "Glass Animals",
+    album: "Dreamland",
+    duration: "3:58",
+    image: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=300&h=300&fit=crop",
+    genre: "Electronic",
+    audioUrl: "https://www.soundjay.com/misc/sounds/bell-ringing-05.wav"
+  },
+  {
+    id: 8,
+    title: "As It Was",
+    artist: "Harry Styles",
+    album: "Harry's House",
+    duration: "2:47",
+    image: "https://images.unsplash.com/photo-1471478331149-c72f17e33c73?w=300&h=300&fit=crop",
+    genre: "Pop",
+    audioUrl: "https://www.soundjay.com/misc/sounds/bell-ringing-05.wav"
+  }
+];
+
+export const genres = ["All", "Pop", "Hip Hop", "Rock", "R&B", "Electronic", "Jazz", "Classical", "Pop Rock", "Dance Pop"];
